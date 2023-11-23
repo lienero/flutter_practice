ac57d21e0a39df5cedeb43a8dacccd4700871f10
lienero <lienero@naver.com>
2023-11-23 16:39:18 +0900

    생성자 연습

--- dart02/bin/dart02.dart
+++ dart02/bin/dart02.dart
@@ -2,8 +2,11 @@ import 'package:dart02/dart02.dart' as dart02;
 
 // class 키워드로 클랫흐 선언
 class Idol {
-  // 클래스 종속 변수 선언
-  String name = '블랙핑크';
+  // 클래스 종속 변수 선언, 생성자 입력 변수는 보통 final
+  final String name;
+  // 생성자 선언, 클래스와 같은 이름이어야 함
+  // this를 사용할 경우 해당되는 변수에 자동으로 매개변수가 저장됨
+  Idol(String name) : this.name = name;
   // 클래스 종속 함수(메서드) 선언
   void sayname() {
     // 클래스 내부 속성 지칭 this
@@ -15,6 +18,8 @@ class Idol {
 
 void main(List<String> arguments) {
   // Idol 인스턴스 생성
-  Idol blackPink = Idol();
+  Idol blackPink = Idol('블랙핑크');
   blackPink.sayname();
+  Idol bts = Idol('BTS');
+  bts.sayname();
 }
