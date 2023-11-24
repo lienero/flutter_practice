52e83d3e57b01513625a9c2094d7983682488021
lienero <lienero@naver.com>
2023-11-24 17:10:15 +0900

    제네릭 사용

--- dart05/bin/dart05.dart
+++ dart05/bin/dart05.dart
@@ -32,9 +32,25 @@ class GirlGroup implements Idol {
   }
 }
 
+// 인스턴스화 할 때 입력받을 타입을 T로 지정
+class Cache<T> {
+  // data의 타입을 추후 입력될 T 탑이르로 지정합니다.
+  final T data;
+
+  Cache({
+    required this.data,
+  });
+}
+
 void main(List<String> arguments) {
   GirlGroup blackPink = GirlGroup('블랙핑크', 4);
 
   blackPink.sayName();
   blackPink.sayMembersCount();
+
+  // T타입을 List<int>로 입력
+  final cache = Cache<List<int>>(data: [1, 2, 3]);
+
+  // 제네릭에 입력된 값을 통해 data 변수의 타입이 자동으로 유추
+  print(cache.data.reduce((value, element) => value + element));
 }
