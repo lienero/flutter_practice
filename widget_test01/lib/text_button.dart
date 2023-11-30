cf3721f269a2af9bac12e9cad40e0051f508ed21
lienero <lienero@naver.com>
2023-11-30 17:13:09 +0900

    텍스트 버튼 실습

---
+++ widget_test01/lib/text_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: TextButton(
+            // 클릭 시 실행
+            onPressed: () {},
+            // 스타일 지정
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+            // 버튼에 넣을 위젯
+            child: Text('텍스트버튼',
+            ),
+          ),
+        ),),
+    );
+  }
+}
