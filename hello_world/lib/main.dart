d539cdc0fe01993422e96647432d468d2408c1a2
lienero <lienero@naver.com>
2023-11-30 16:35:56 +0900

    flutter 입문용 hello_world앱 생성

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    // 머티리얼 디자인 위젯
+    MaterialApp(
+      // Scaffold 위젯
+      home: Scaffold(
+        // Text 위젯
+        body: Text(
+          'Hello Code Factory',
+        ),
+      ),
+    ),
+  );
+}
