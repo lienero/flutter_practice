0f90b6c565334395e93d8a738250c0294f2e9b36
lienero <lienero@naver.com>
2023-12-01 15:10:15 +0900

    floating action 버튼 실습

---
+++ widget_test01/lib/floating_action_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FloatingActionButtonExample());
+}
+
+class FloatingActionButtonExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+          // 클릭했을 때 실행할 함수
+          onPressed: () {},
+          child: Text('클릭'),
+        ),
+        body: Container(),
+        ),
+    );
+  }
+}
