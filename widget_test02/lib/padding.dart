182f1fe3673127da57e79fdda46871e17b61d062
lienero <lienero@naver.com>
2023-12-01 16:12:32 +0900

    마진 기능 실습

--- widget_test02/lib/padding.dart
+++ widget_test02/lib/padding.dart
@@ -11,20 +11,28 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           body: Center(
             // Container: 다른 위젯을 담는 데 사용되는 위젯
+            // 최상위 검정 컨테이너 (margin이 적용되는 대상)
               child: Container(
-                color: Colors.blue,
-                child: Padding(
+                color: Colors.black,
+                // 중간 파랑 컨테이너
+                child: Container (
+                  color: Colors.blue,
+                  // 바깥에 간격을 추가하는 마직 적용
+                  margin: EdgeInsets.all(16),
+                  child: Padding(
                   // 상하, 좌우로 모두 16픽셀만큼 패딩 적용
                   padding: EdgeInsets.all(
                     16,
                   ),
+                  // 패딩이 적용된 빨강 컨테이너
                   child: Container(
                     color: Colors.red,
                     width: 50,
                     height: 50,
                   ),
                 ),
-              )
+              ),
+            ),
           ),
         )
     );
