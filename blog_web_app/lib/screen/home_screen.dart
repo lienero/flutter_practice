98fde40c86b360598b18b8f70df209a117f5fbdf
lienero <lienero@naver.com>
2023-12-05 13:44:41 +0900

    앱바 구현

--- blog_web_app/lib/screen/home_screen.dart
+++ blog_web_app/lib/screen/home_screen.dart
@@ -7,6 +7,14 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar( // 앱바 위젯 추가
+        // 배경색 지정
+        backgroundColor: Colors.orange,
+        // 앱 타이틀 설정
+        title: Text('Code Factory'),
+        // 가운데 정렬
+        centerTitle: true, 
+      ),
       body: Text('Home Screen'),
     );
   }
