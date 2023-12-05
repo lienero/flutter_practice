7062686e186fb30478910a79e4d5f857d3f4dd7b
lienero <lienero@naver.com>
2023-12-05 15:59:40 +0900

    페이지 뷰 구현 및 버그 수정

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -7,7 +7,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      // PAGEVIEW 추가
+      body: PageView(
+        // 샘플 리스트 생성
+        children: [1,2,3,4,5].map( // 위젯으로 매핑
+              (number) => Image.asset(
+                  'asset/img/image_$number.jpeg',
+                  // BoxFit.cover 최대한 전체 화면을 차지하도록 늘어남
+                  fit: BoxFit.cover,),
+        ).toList(),
+      ),
     );
   }
 }
