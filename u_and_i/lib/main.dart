6dfb47f53c1c03a24f9c65a6481a357c6560c519
lienero <lienero@naver.com>
2023-12-05 17:08:42 +0900

    홈스크린 ui 구현 및 스타일 지정

--- u_and_i/lib/main.dart
+++ u_and_i/lib/main.dart
@@ -4,6 +4,30 @@ import 'package:u_and_i/screen/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData( // 테마를 지정할 수 있는 클래스
+        fontFamily: 'sunflower', // 기본 글씨체
+        textTheme: TextTheme( // 글자 테마를 적용할 수 있는 클래스
+          headline1: TextStyle( // headline1 스타일 정의
+            color: Colors.white,
+            fontSize: 80,
+            fontWeight: FontWeight.w700,
+            fontFamily: 'parisienne'
+          ),
+          headline2: TextStyle(
+            color: Colors.white,
+            fontSize: 50,
+            fontWeight: FontWeight.w700
+          ),
+          bodyText1: TextStyle(
+            color: Colors.white,
+            fontSize: 30
+          ),
+          bodyText2: TextStyle(
+            color: Colors.white,
+            fontSize: 20
+          )
+        )
+      ),
       home: HomeScreen(),
     ),
   );
--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -6,7 +6,22 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.pink[100],
+      // 시스템 UI 피해서 UI 그리기
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          // 위 아래 끝에 위젯 배치
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // 반대축 최대 크기로 늘리기
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDay(),
+            _CoupleImage(),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -15,13 +30,44 @@ class HomeScreen extends StatelessWidget {
 class _DDay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('DDay Widget');
+    final textTheme = Theme.of(context).textTheme;
+    return Column(
+      children: [
+        const SizedBox(height: 16),
+        Text('U&I', style: textTheme.headline1,),
+        const SizedBox(height: 16),
+        Text('우리 처음 만난 날', style: textTheme.bodyText1,),
+        Text('2021.11.23', style: textTheme.bodyText2,),
+        const SizedBox(height: 16),
+        IconButton(
+            iconSize: 60,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            )
+        ),
+        const SizedBox(height: 16),
+        Text('D+365', style: textTheme.headline2,),
+      ],
+    );
   }
 }
 
 class _CoupleImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('Couple Image Widget');
+    // 이미지가 남는 공간만큼 차지하도록 Expanded 위젯 사용
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+          // MediaQuery.of(context) : 화면 크기와 관련된 각종 기능을 사용
+          // of(context) : 현재 위젯 트리에 있는 가장 가까이에 있는 MediaQuery 값
+          // 화면의 반만큼 높이 구현
+          // Expandend가 우선 순위를 갖게 되어 무시됨
+          height: MediaQuery.of(context).size.height / 2,),
+      )
+    );
   }
 }
