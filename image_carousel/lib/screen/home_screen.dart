54a4fda1bcf7b0d23266c71f382a38a9b3cf0b35
lienero <lienero@naver.com>
2023-12-05 16:17:03 +0900

    타이머 추가하기

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+// StatefulWidget 위젯 정의
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+// _HomeScreenState 정의
+class _HomeScreenState extends State<HomeScreen> {
+  // PageController 생성
+  final PageController pageController = PageController();
+  // initState() 함수 등록
+  @override
+  void initState() {
+    // 부모 initState() 실행
+    super.initState();
+    // Timer.periodic() 등록
+    Timer.periodic(
+        Duration(seconds: 3),
+        (timer) {
+          // 현재 페이지 가져오기
+          int? nextPage = pageController.page?.toInt();
+          if (nextPage == null) { // 페이지 값이 없을 때 예외처리
+            return;
+          }
+          if (nextPage == 4) { // 첫 페이지와 마지막 페이지 분기 처리
+            nextPage = 0;
+          } else {
+            nextPage++;
+          }
+          pageController.animateToPage( // 페이지 변경
+              nextPage,
+              duration: Duration(milliseconds: 500),
+              curve: Curves.ease,
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // 상태바 색상 변경
@@ -12,12 +49,15 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       // PAGEVIEW 추가
       body: PageView(
+        // PageController 등록
+        controller: pageController,
         // 샘플 리스트 생성
         children: [1,2,3,4,5].map( // 위젯으로 매핑
               (number) => Image.asset(
                   'asset/img/image_$number.jpeg',
                   // BoxFit.cover 최대한 전체 화면을 차지하도록 늘어남
-                  fit: BoxFit.cover,),
+                  fit: BoxFit.cover,
+              ),
         ).toList(),
       ),
     );
