735666821c6c50bb4386c68305934f7be4f8e619
lienero <lienero@naver.com>
2023-12-14 14:05:19 +0900

    slider 위젯과 동영상 연동

--- vid_player/lib/component/custom_video_player.dart
+++ vid_player/lib/component/custom_video_player.dart
@@ -49,7 +49,27 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     }
     return AspectRatio( // 동영상 비율에 따른 화면 렌더링
       aspectRatio: videoController!.value.aspectRatio,
-      child: VideoPlayer(videoController!),
+      child: Stack( // chidren 위ㅔ짓을 위로 쌓을 수 있는 위젯
+        children: [
+          VideoPlayer(videoController!),
+          Positioned( // child 위젯 위치를 정할 수 있는 위젯
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Slider(
+              onChanged: (double val) { // 슬라이더가 이동할 때 마다 실행
+                videoController!.seekTo( // seekTo함수는 동영상의 재생 위치를 특정 위치로 이동
+                  Duration(seconds: val.toInt())
+                );
+              },
+              // 동영상 재생 위치를 초 단위로 표현
+              value: videoController!.value.position.inSeconds.toDouble(),
+              min: 0,
+              // max 매개변수에는 double 값을 입력해야함
+              max: videoController!.value.duration.inSeconds.toDouble(),
+            ))
+        ],
+      )
     );
   }
 }
