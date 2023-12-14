e48e0de87bd3cc553b8247b10f8396bb8ef7af70
lienero <lienero@naver.com>
2023-12-14 14:54:16 +0900

    컨트롤러 감추기 기능 추가

--- vid_player/lib/component/custom_video_player.dart
+++ vid_player/lib/component/custom_video_player.dart
@@ -23,6 +23,7 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  bool showControls = false; // 동영상 조작하는 아이콘을 보일지 여부
   // 동영상을 조작하는 컨트롤러
   VideoPlayerController? videoController;
 
@@ -83,59 +84,70 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
         child: CircularProgressIndicator(),
       );
     }
-    return AspectRatio( // 동영상 비율에 따른 화면 렌더링
-      aspectRatio: videoController!.value.aspectRatio,
-      child: Stack( // chidren 위ㅔ짓을 위로 쌓을 수 있는 위젯
-        children: [
-          VideoPlayer(videoController!),
-          Positioned( // child 위젯 위치를 정할 수 있는 위젯
-            bottom: 0,
-            right: 0,
-            left: 0,
-            child: Slider(
-              onChanged: (double val) { // 슬라이더가 이동할 때 마다 실행
-                videoController!.seekTo( // seekTo함수는 동영상의 재생 위치를 특정 위치로 이동
-                  Duration(seconds: val.toInt())
-                );
-              },
-              // 동영상 재생 위치를 초 단위로 표현
-              value: videoController!.value.position.inSeconds.toDouble(),
-              min: 0,
-              // max 매개변수에는 double 값을 입력해야함
-              max: videoController!.value.duration.inSeconds.toDouble(),
-            )
-          ),
-          Align( // 오른쪽 위에 새 동영상 아이콘 위치
-            alignment: Alignment.topRight,
-            child: CustomIconButton(
-              // 카메라 아이콘을 선택하면 새로운 동영상 선택 함수 실행
-              onPressed: widget.onNewVideoPressed,
-              iconData: Icons.photo_camera_back,
+    return GestureDetector( // 화면 전체의 탭을 인식하기 위해 사용
+      onTap: () {
+        setState(() {
+          showControls = !showControls;
+        });
+      },
+      child: AspectRatio( // 동영상 비율에 따른 화면 렌더링
+        aspectRatio: videoController!.value.aspectRatio,
+        child: Stack( // chidren 위젯을 위로 쌓을 수 있는 위젯
+          children: [
+            VideoPlayer(videoController!),
+            if (showControls)
+              Container(color: Colors.black.withOpacity(0.5)),
+            Positioned( // child 위젯 위치를 정할 수 있는 위젯
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Slider(
+                onChanged: (double val) { // 슬라이더가 이동할 때 마다 실행
+                  videoController!.seekTo( // seekTo함수는 동영상의 재생 위치를 특정 위치로 이동
+                    Duration(seconds: val.toInt())
+                  );
+                },
+                // 동영상 재생 위치를 초 단위로 표현
+                value: videoController!.value.position.inSeconds.toDouble(),
+                min: 0,
+                // max 매개변수에는 double 값을 입력해야함
+                max: videoController!.value.duration.inSeconds.toDouble(),
+              )
             ),
-          ),
-          Align( // 동영상 재생 관련 아이콘 중앙에 위치
-            alignment: Alignment.center,
-            child: Row(
-              // spaceEvenly : 아이콘 간격을 동일하게 설정
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CustomIconButton( // 되감기 버튼
-                  onPressed: onReversePressed,
-                  iconData: Icons.rotate_left,
+            if (showControls)
+              Align( // 오른쪽 위에 새 동영상 아이콘 위치
+                alignment: Alignment.topRight,
+                child: CustomIconButton(
+                  // 카메라 아이콘을 선택하면 새로운 동영상 선택 함수 실행
+                  onPressed: widget.onNewVideoPressed,
+                  iconData: Icons.photo_camera_back,
                 ),
-                CustomIconButton( // 재생 버튼
-                  onPressed: onPlayPressed,
-                  iconData: videoController!.value.isPlaying ?
-                  Icons.pause : Icons.play_arrow,
+              ),
+            if (showControls)
+              Align( // 동영상 재생 관련 아이콘 중앙에 위치
+                alignment: Alignment.center,
+                child: Row(
+                  // spaceEvenly : 아이콘 간격을 동일하게 설정
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CustomIconButton( // 되감기 버튼
+                      onPressed: onReversePressed,
+                      iconData: Icons.rotate_left,
+                    ),
+                    CustomIconButton( // 재생 버튼
+                      onPressed: onPlayPressed,
+                      iconData: videoController!.value.isPlaying ?
+                      Icons.pause : Icons.play_arrow,
+                    ),
+                    CustomIconButton( // 앞으로 감기 버튼
+                      onPressed: onForwardPressed,
+                      iconData: Icons.rotate_right,
+                    ),
+                  ],
                 ),
-                CustomIconButton( // 앞으로 감기 버튼
-                  onPressed: onForwardPressed,
-                  iconData: Icons.rotate_right,
-                ),
-              ],
-            ),
-          )
-        ],
+              ),
+          ],
+        )
       )
     );
   }
