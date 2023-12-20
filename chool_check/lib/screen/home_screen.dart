671df6778ff4aaea8990e12bf4c9904fa51dfe54
lienero <lienero@naver.com>
2023-12-20 13:49:43 +0900

    버그 수정 및 바디구현

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  static final LatLng companyLatLng = LatLng( // 지도 초기화 위치
+    37.5233273, // 위도
+    126.921252, // 경도
+  );
+
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Text('Home Screen'),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: companyLatLng,
+          zoom: 16 // 확대 정도 (높을수록 크게 보임)
+        ),
+      ),
     );
   }
 
+  // AppBar 구현 함수
   AppBar renderAppBar() {
     return AppBar(
       centerTitle: true,
