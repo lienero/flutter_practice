b3565100fb48c60ff5f41474898636a0470ac273
lienero <lienero@naver.com>
2023-12-21 16:37:56 +0900

    AppBar 구현하기

--- image_editor/lib/screen/home_screen.dart
+++ image_editor/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_editor/component/main_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,7 +7,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: Stack( // 스크린에 Body, AppBar, Footer 순서로 쌓을 준비
+        fit: StackFit.expand, // 자식 위젯들 최대 크기로 펼치기
+        children: [
+          Positioned( // MainAppBar를 좌, 우, 위 끝에 정렬
+            top: 0,
+            left: 0,
+            right: 0,
+            child: MainAppBar(
+              onPickImage: onPickImage,
+              onSaveImage: onSaveImage,
+              onDeleteImage: onDeleteImage,
+            ),
+          ),
+        ],
+      ),
     );
   }
+
+  void onPickImage() {}
+
+  void onSaveImage() {}
+
+  void onDeleteImage() {}
 }
