a830133d5334d9a006610a84d43c9e46f17c003d
lienero <lienero@naver.com>
2023-12-22 15:19:47 +0900

    커스텀 유튜브 플레이어 위젯 구현

--- cf_tube/lib/screen/home_screen.dart
+++ cf_tube/lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cf_tube/component/custom_youtube_player.dart';
+import 'package:cf_tube/model/video_model.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,7 +8,13 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.black,
+      body: CustomYoutubePlayer(
+        videoModel: VideoModel(
+          id: '3Ck42C2ZCb8', // 샘플 동영상 id
+          title: '다크 언어 기본기 1시간만에 끝내기' // 샘플 제목
+        ),
+      ),
     );
   }
 }
