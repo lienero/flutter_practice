c8de04a422906f48c4be39d265d6fa4386e60dad
lienero <lienero@naver.com>
2023-12-22 14:27:12 +0900

    이미지 저장하기 기능 추가

--- image_editor/lib/screen/home_screen.dart
+++ image_editor/lib/screen/home_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:image_editor/model/sticker_model.dart';
 import 'package:image_editor/component/main_app_bar.dart';
 import 'package:image_editor/component/footer.dart';
 import 'package:image_editor/component/emoticon_sticker.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:uuid/uuid.dart';
+import 'package:flutter/services.dart';
 import 'dart:io';
+import 'dart:ui' as ui;
+import 'dart:typed_data';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,6 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   XFile? image; // 선택한 이미지를 저장할 변수
   Set<StickerModel> stickers = {}; // 화면에 추가된 스티커를 저장할 변수
   String? selectedId; // 현재 선택된 스티커의 ID
+  GlobalKey imgKey = GlobalKey(); // 이미지로 전환할 위젯에 입력해줄 키값
 
   @override
   Widget build(BuildContext context) {
@@ -52,32 +59,36 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Widget renderBody() {
     if (image != null) {
-      return Positioned.fill( // Stack 크기의 최대 크기 만큼 차지하기
-          child: InteractiveViewer( // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
-            child: Stack(
-              fit: StackFit.expand, // 크기 최대로 늘려주기
-              children: [
-                Image.file(
-                  File(image!.path),
-                  fit: BoxFit.cover, // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
-                ),
-                ...stickers.map(
-                    (sticker) => Center( // 최초 스티커 선택 시 중앙에 배치
-                      child: EmoticonSticker(
-                        key: ObjectKey(sticker.id),
-                        onTransfrom: () {
-                          // 스티커의 ID값 함수의 매개변수로 전달
-                          onTransform(sticker.id);
-                          },
-                        imgPath: sticker.imgPath,
-                        isSelected: selectedId == sticker.id,
-                      ),
+      // RepaintBoundary : 수치는 낮지만 일부분의 CPU와 메모리 영역을 사용해 렌더링 최적화
+      return RepaintBoundary( // 위젯을 이미지로 저장하는 데 사용
+        key: imgKey,
+        child: Positioned.fill( // Stack 크기의 최대 크기 만큼 차지하기
+            child: InteractiveViewer( // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
+                child: Stack(
+                  fit: StackFit.expand, // 크기 최대로 늘려주기
+                  children: [
+                    Image.file(
+                      File(image!.path),
+                      fit: BoxFit.cover, // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
+                    ),
+                    ...stickers.map(
+                            (sticker) => Center( // 최초 스티커 선택 시 중앙에 배치
+                          child: EmoticonSticker(
+                            key: ObjectKey(sticker.id),
+                            onTransfrom: () {
+                              // 스티커의 ID값 함수의 매개변수로 전달
+                              onTransform(sticker.id);
+                            },
+                            imgPath: sticker.imgPath,
+                            isSelected: selectedId == sticker.id,
+                          ),
+                        )
                     )
-                )
 
-              ],
+                  ],
+                )
             )
-          )
+        ),
       );
     } else {
       return Center( // 이미지 선택이 안 된 경우 이미지 선택 버튼 표시
@@ -121,7 +132,24 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void onSaveImage() {}
+  void onSaveImage() async {
+    RenderRepaintBoundary boundary = imgKey.currentContext!
+        .findRenderObject() as RenderRepaintBoundary; // 실제 화면에 렌더링된 위젯 찾기
+    // 바운더리를 이미지로 변환
+    ui.Image image = await boundary.toImage();
+    // byteData 형태로 형태 변경
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    // Uint8List 현태로 형태 변경
+    Uint8List pngByte = byteData!.buffer.asUint8List();
+
+    // 이미지 저장하기
+    await ImageGallerySaver.saveImage(pngByte, quality: 100);
+    ScaffoldMessenger.of(context).showSnackBar( // 저장 후 snackbar 부여주기
+      SnackBar(
+        content: Text('저장되었습니다!'),
+      )
+    );
+  }
 
   void onDeleteImage() async {
     setState(() {
