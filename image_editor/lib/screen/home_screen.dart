a1db486e3fa4ce0f810e9a03cd745d3c1e1bf7ee
lienero <lienero@naver.com>
2023-12-22 14:06:53 +0900

    스티커 붙이기 기능 추가

--- image_editor/lib/screen/home_screen.dart
+++ image_editor/lib/screen/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:image_editor/model/sticker_model.dart';
 import 'package:image_editor/component/main_app_bar.dart';
 import 'package:image_editor/component/footer.dart';
+import 'package:image_editor/component/emoticon_sticker.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 import 'dart:io';
 
 class HomeScreen extends StatefulWidget {
@@ -13,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   XFile? image; // 선택한 이미지를 저장할 변수
+  Set<StickerModel> stickers = {}; // 화면에 추가된 스티커를 저장할 변수
+  String? selectedId; // 현재 선택된 스티커의 ID
 
   @override
   Widget build(BuildContext context) {
@@ -49,10 +54,29 @@ class _HomeScreenState extends State<HomeScreen> {
     if (image != null) {
       return Positioned.fill( // Stack 크기의 최대 크기 만큼 차지하기
           child: InteractiveViewer( // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
-            child: Image.file(
-              File(image!.path),
-              fit: BoxFit.cover, // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
-            ),
+            child: Stack(
+              fit: StackFit.expand, // 크기 최대로 늘려주기
+              children: [
+                Image.file(
+                  File(image!.path),
+                  fit: BoxFit.cover, // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
+                ),
+                ...stickers.map(
+                    (sticker) => Center( // 최초 스티커 선택 시 중앙에 배치
+                      child: EmoticonSticker(
+                        key: ObjectKey(sticker.id),
+                        onTransfrom: () {
+                          // 스티커의 ID값 함수의 매개변수로 전달
+                          onTransform(sticker.id);
+                          },
+                        imgPath: sticker.imgPath,
+                        isSelected: selectedId == sticker.id,
+                      ),
+                    )
+                )
+
+              ],
+            )
           )
       );
     } else {
@@ -68,7 +92,25 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void onEmotcionTap(int index) {}
+  void onTransform(String id) {
+    // 스티커가 변형될 때마다 변형 중인
+    // 스티커를 현재 선택한 스티커로 지정
+    setState(() {
+      selectedId = id;
+    });
+  }
+
+  void onEmotcionTap(int index) async {
+    setState(() {
+      stickers = {
+        ...stickers,
+        StickerModel(
+          id: Uuid().v4(), // 스티커의 고유 ID 생성
+          imgPath: 'asset/img/emoticon_$index.png'
+        ),
+      };
+    });
+  }
 
   void onPickImage() async {
     // 갤러리에서 이미지 선택
