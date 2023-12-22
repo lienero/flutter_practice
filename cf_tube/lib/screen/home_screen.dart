5dbfc6f8605fca61a82e5562e139fa107f020e72
lienero <lienero@naver.com>
2023-12-22 15:56:15 +0900

    동영상 리스트 위젯 구현

--- cf_tube/lib/screen/home_screen.dart
+++ cf_tube/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cf_tube/component/custom_youtube_player.dart';
 import 'package:cf_tube/model/video_model.dart';
+import 'package:cf_tube/repository/youtube_repository.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,12 +10,35 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: CustomYoutubePlayer(
-        videoModel: VideoModel(
-          id: '3Ck42C2ZCb8', // 샘플 동영상 id
-          title: '다크 언어 기본기 1시간만에 끝내기' // 샘플 제목
-        ),
+      appBar: AppBar(
+        centerTitle: true, // 제목 가운데 정렬
+        title: Text('코팩튜브'),
+        backgroundColor: Colors.black,
       ),
+      body: FutureBuilder<List<VideoModel>>(
+        future: YoutubeRespository.getVideos(), // 유튜브 영상 가져오기
+        builder: (context, snapshot) {
+          if (snapshot.hasError) { // 에러가 있을 경우 에러 화면에 표시
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          }
+
+          if (!snapshot.hasData) { // 로딩 중일때 로딩 위젯 보여주기
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView( // List<VideoModel>을 CustomYotubePlayer로 매핑
+            // 아래로 당겨서 스크롤할 떄 튕기는 애니메이션 추가
+            physics: BouncingScrollPhysics(),
+            children: snapshot.data!
+              .map((e) => CustomYoutubePlayer(videoModel: e))
+              .toList(),
+          );
+        }
+      )
     );
   }
 }
