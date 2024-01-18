d7b4c990858f0d463f3c5b183a8d820aa8efdb02
lienero <lienero@naver.com>
2024-01-18 15:41:18 +0900

    파이어베이스 설정 설정하기

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -6,10 +6,17 @@ import 'package:get_it/get_it.dart';
 import 'package:calendar_scheduler/provider/schedule_provider.dart';
 import 'package:calendar_scheduler/repository/schedule_repository.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:calendar_scheduler/firebase_options.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 파이어베이스 프로젝트 설정 함수
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
   
