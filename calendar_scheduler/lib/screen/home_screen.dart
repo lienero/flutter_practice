f6dd5c036e8141aebbfae331c414facafb1cce53
lienero <lienero@naver.com>
2024-01-18 16:48:37 +0900

    일정 데이터 조회하기

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 import 'package:calendar_scheduler/const/colors.dart';
 import 'package:calendar_scheduler/component/main_calendar.dart';
 import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
 import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
-import 'package:calendar_scheduler/database/drift_database.dart';
-import 'package:provider/provider.dart';
-import 'package:calendar_scheduler/provider/schedule_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:calendar_scheduler/model/schedule_model.dart';
+import 'package:intl/intl.dart';
 
 // 프로바이더로 상태를 관리하기에 statefulwidget 사용하지 않음
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   DateTime selectedDate = DateTime.utc( // 선택된 날짜를 관리할 변수
     DateTime.now().year,
     DateTime.now().month,
@@ -19,12 +24,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 프로바이더 변경이 있을 때 마다 build 함수 재실행
-    final provider = context.watch<ScheduleProvider>();
-    // 선택된 날짜 가져오기
-    final selectedDate = provider.selectedDate;
-    // 선택된 날짜에 해당하는 일정들 가져오기
-    final schedules = provider.cache[selectedDate] ?? [];
 
     return Scaffold(
       floatingActionButton: FloatingActionButton( // 새 일정 버튼
@@ -52,39 +51,74 @@ class HomeScreen extends StatelessWidget {
                 onDaySelected(selectedDate, focusedDate, context),
             ),
             SizedBox(height: 8),
-            TodayBanner( // build() 함수 내부의 TodayBanner 위젯
-              selectedDate: selectedDate,
-              count: schedules.length,
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                .collection('schedule')
+                .where(
+                  'date',
+                  isEqualTo: '${DateFormat('yyyyMMdd').format(selectedDate)}',
+                )
+                .snapshots(),
+              builder: (context, snapshot) {
+                return TodayBanner( // build() 함수 내부의 TodayBanner 위젯
+                  selectedDate: selectedDate,
+                  count: snapshot.data?.docs.length ?? 0,
+                );
+              }
             ),
             SizedBox(height: 8),
             Expanded( // 남는 공간 모두 차지하기
-              // streambuiler를 프로바이더로 대체
-              child: ListView.builder( // 화면에 보이는 값들만 렌더링하는 리스트
-                // 리스트에 입력할 값들의 총 개수
-                itemCount: schedules.length,
-                itemBuilder: (context, index) {
-                  // 현재 인덱스에 해당되는 일정
-                  final schedule = schedules[index];
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                  .collection('schedule')
+                  .where(
+                    'date',
+                    isEqualTo: '${DateFormat('yyyyMMdd').format(selectedDate)}',
+                  )
+                  .snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text('일정 정보를 가져오지 못했습니다.'),
+                    );
+                  }
 
-                  return Dismissible( // Dismissible 위젯을 사용하면 제스처 기능 지원
-                    key: ObjectKey(schedule.id), // 유니크한 키값
-                    // 밀기 방향(오른쪽에서 왼쪽으로)
-                    direction: DismissDirection.startToEnd,
-                    // 밀기 했을 때 실행할 함수
-                    onDismissed: (DismissDirection direction) {
-                      provider.deleteSchedule(date: selectedDate, id: schedule.id);
-                    },
-                    child: Padding( // 좌우로 패딩을 추가해서 ui 개선
-                      padding: const EdgeInsets.only(bottom: 8, left: 8, right: 8),
-                      child: ScheduleCard(
-                        startTime: schedule.startTime,
-                        endTime: schedule.endTime,
-                        content: schedule.content,
-                      ),
-                    ),
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Container();
+                  }
+
+                  final schedules = snapshot.data!.docs.map(
+                    (QueryDocumentSnapshot e) => ScheduleModel.fromJson(
+                      json: (e.data() as Map<String, dynamic>),
+                    )
+                  ).toList();
+
+                  return ListView.builder( // 화면에 보이는 값들만 렌더링하는 리스트
+                    // 리스트에 입력할 값들의 총 개수
+                    itemCount: schedules.length,
+                    itemBuilder: (context, index) {
+                      // 현재 인덱스에 해당되는 일정
+                      final schedule = schedules[index];
+
+                      return Dismissible( // Dismissible 위젯을 사용하면 제스처 기능 지원
+                        key: ObjectKey(schedule.id), // 유니크한 키값
+                        // 밀기 방향(오른쪽에서 왼쪽으로)
+                        direction: DismissDirection.startToEnd,
+                        // 밀기 했을 때 실행할 함수
+                        onDismissed: (DismissDirection direction) { },
+                        child: Padding( // 좌우로 패딩을 추가해서 ui 개선
+                          padding: const EdgeInsets.only(bottom: 8, left: 8, right: 8),
+                          child: ScheduleCard(
+                            startTime: schedule.startTime,
+                            endTime: schedule.endTime,
+                            content: schedule.content,
+                          ),
+                        ),
+                      );
+                    }
                   );
-                }
-              ),
+                },
+              )
             ),
           ],
         ),
@@ -98,8 +132,8 @@ class HomeScreen extends StatelessWidget {
     DateTime focusedDate,
     BuildContext context,
   ) {
-    final provider = context.read<ScheduleProvider>();
-    provider.changeScheduleDate(date: selectedDate);
-    provider.getSchedules(date: selectedDate);
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
   }
 }
