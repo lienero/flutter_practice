a23daa796bb2690866f55a3ac3dae626b33fc447
lienero <lienero@naver.com>
2024-01-18 17:05:59 +0900

    메인파일 코드 정리하기

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:calendar_scheduler/database/drift_database.dart';
-import 'package:get_it/get_it.dart';
-import 'package:calendar_scheduler/provider/schedule_provider.dart';
-import 'package:calendar_scheduler/repository/schedule_repository.dart';
-import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:calendar_scheduler/firebase_options.dart';
 
@@ -20,21 +15,8 @@ void main() async {
   
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
   
-  final database = LocalDatabase(); // 데이터 베이스 생성
-  
-  // GetIt에 데이터베이스 변수 주입
-  // GetIt 패키지는 의존성 주입을 구현하는 플러그인, 프로젝트 어디서든 사용가능
-  GetIt.I.registerSingleton<LocalDatabase>(database);
-
-  final repository = ScheduleRepository();
-  final scheduleProvider = ScheduleProvider(repository: repository);
-  
-  runApp(
-    ChangeNotifierProvider( // Provider 하위 위젯에 제공하기
-      create: (_) => scheduleProvider,
-      child: MaterialApp(
-        home: HomeScreen(),
-      ),
-    )
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
   );
 }
